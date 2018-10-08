3ce0bcc452d470592ab3a86f92e6a02347377150
Paul Müller <dev@craban.de>
2018-10-08 22:02:47 +0200

    play around with dmgs

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "Please specify package name as command line argument!"
+    exit 1
+fi
+
+ls -l ./dist
+
+NAME=$1
+SCRIPT="./${1}/__main__.py"
+APP="./dist/${1}.app"
+DMG="./dist/${1}.dmg"
+
 pip install pyinstaller
 
-pyinstaller -w -y -n fooqt ./fooqt/__main__.py
+pyinstaller -w -y -n $NAME $SCRIPT
 
-cat fooqt.spec
+hdiutil create -srcfolder "${APP}" -volname "${NAME}" -fs HFS+ \
+        -fsargs "-c c=64,a=16,e=16" -format UDRW pack.temp.dmg
+
+hdiutil convert pack.temp.dmg -format UDZO -imagekey zlib-level=9 \
+        -o "${DMG}"
+
+ls -l ./dist
