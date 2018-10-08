c35ccc06e27c707aa811aea36a1ea5b276bf2a79
Paul Müller <dev@craban.de>
2018-10-08 22:18:59 +0200

    setup first release

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -5,8 +5,6 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-ls -l ./dist
-
 NAME=$1
 SCRIPT="./${1}/__main__.py"
 APP="./dist/${1}.app"
@@ -16,10 +14,15 @@ pip install pyinstaller
 
 pyinstaller -w -y -n $NAME $SCRIPT
 
+# create temporary DMG
 hdiutil create -srcfolder "${APP}" -volname "${NAME}" -fs HFS+ \
         -fsargs "-c c=64,a=16,e=16" -format UDRW ./dist/pack.temp.dmg
 
-hdiutil convert ./dist/pack.temp.dmg -format UDZO -imagekey zlib-level=9 \
-        -o "${DMG}"
+# optional: edit the DMG
+# https://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools
 
+# create crompressed DMG
+hdiutil convert ./dist/pack.temp.dmg -format UDZO -imagekey zlib-level=9 -o "${DMG}"
+
+# show created files
 ls -l ./dist
