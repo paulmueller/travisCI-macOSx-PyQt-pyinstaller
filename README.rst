0f10de0bf8e19a8c8aaad0af9c72ee4930ac844f
Paul Müller <dev@craban.de>
2018-10-08 18:56:39 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+travisCI-macOSx-PyQt-pyinstaller
+================================
+
+|Build Status|
+
+Minimal setup for building PyQt apps for MacOSx on travisCI with PyInstaller
+
+.. |Build Status| image:: http://img.shields.io/travis/paulmueller/travisCI-macOSx-PyQt-pyinstaller.svg
+   :target: https://travis-ci.org/paulmueller/travisCI-macOSx-PyQt-pyinstaller/
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os.path import exists, dirname, realpath
+from setuptools import setup, Extension, find_packages
+import sys
+
+author = u"Paul Müller"
+authors = [author]
+description = 'Minimal setup for building PyQt apps for MacOSx on travisCI'
+name = 'fooqt'
+year = "2018"
+
+
+sys.path.insert(0, realpath(dirname(__file__))+"/"+name)
+try:
+    from _version import version  # @UnresolvedImport
+except:
+    version = "unknown"
+
+setup(
+    name=name,
+    author=author,
+    author_email='dev@craban.de',
+    url='https://github.com/paulmueller/travisCI-macOSx-PyQt-pyinstaller',
+    version=version,
+    packages=find_packages(),
+    package_dir={name: name},
+    include_package_data=True,
+    license="MIT",
+    description=description,
+    long_description=open('README.rst').read() if exists('README.rst') else '',
+    ext_modules = extensions,
+    keywords=["foo-bar"],
+    setup_requires=['pytest-runner'],
+    tests_require=["pytest"],
+    platforms=['ALL'],
+    )
