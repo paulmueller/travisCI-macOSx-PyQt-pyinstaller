27532e363322b98c625fcaca8483eb366e6bf623
Paul Müller <dev@craban.de>
2018-10-09 22:12:40 +0200

    update hook

--- .travis/hook-fooqt.py
+++ .travis/hook-fooqt.py
@@ -17,11 +17,13 @@ from PyInstaller.utils.hooks import collect_data_files
 datas = collect_data_files("fooqt")
 datas += collect_data_files("fooqt", subdir="img")
 
-
 # Windows specific
 if platform.system()=="Windows":
-    # The build machine should have this variable set
-    # (see Readme.rst)
+    # The build machine should have this variable set:
+    # Set the environment variable QT_QPA_PLATFORM_PLUGIN_PATH
+    # to a location containing "qwindows.dll"
+    # (usually in Anaconda\Library\plugins\platforms) to resolve:
+    # Error: 'could not find or load the Qt platform plugin "windows"'
     dllsource = os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"]
     for ds in dllsource.split(";"):
         datas += [(ds+"/*.dll", "platforms")]
