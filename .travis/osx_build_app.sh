2333032d730744a0e55dfce76a9b988e3c82038b
Paul Müller <dev@craban.de>
2019-07-03 08:37:40 +0200

    correct app name

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -23,7 +23,7 @@ fi
 
 # append "App" to avoid naming conflicts with python library
 SCRIPT=".travis/${NAME}App.py"
-APP="./dist_app/${NAME}.app"
+APP="./dist_app/${NAME}App.app"
 DMG="./dist_app/${NAMEVERSION}.dmg"
 TMP="./dist_app/pack.temp.dmg"
 pip install pyinstaller
