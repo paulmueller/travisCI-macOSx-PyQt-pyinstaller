1511d6a4f0b215c0d16872b77ca2e2ed335ea95e
Paul Müller <dev@craban.de>
2020-06-03 10:42:16 +0200

    change script

--- .travis/osx_build_app.sh
+++ .travis/osx_build_app.sh
@@ -41,7 +41,8 @@ pip install pyinstaller
 pyinstaller -w -y --distpath="./dist_app" --additional-hooks-dir=".travis" $SCRIPT
 
 # Test the binary by executing it with --version argument
-echo "\nTesting the app (this should print the version). If it hangs, set 'console=True' in your .spec file!"
+echo ""
+echo "...Testing the app (this should print the version). If it hangs, set 'console=True' in your .spec file!"
 ./dist_app/${NAME}.app/Contents/MacOS/${NAME} --version
 
 # Create PKG (pkgbuild is for deployments in app stores)
